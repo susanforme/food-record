5ba6efbbfdf1e263b8f15085601b2aaec6757f85
Spring  Breeze <rzc307853639@gmail.com>
2021-01-26 19:02:52 +0800

    vite配置

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const del = require('del');
+// 编译同时复制到dist目录
+function copy() {
+  return gulp.src('src/graphql/gql/*').pipe(gulp.dest('dist/graphql/gql'));
+}
+
+function clean(cb) {
+  return del(['dist', 'yarn-error.log'], cb);
+}
+// 清除快照
+function cleanTest(cb) {
+  return del(['test/__snapshots__/*'], cb);
+}
+module.exports = {
+  default: copy,
+  copy,
+  clean,
+  cleanTest,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+});
